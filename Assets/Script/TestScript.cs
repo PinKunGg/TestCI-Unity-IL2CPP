88758e496b91ececefc028d76d459ce4d0f1392b
nattapat.s_nb <nattapat.s@varisoft.com>
2024-01-09 23:13:41 +0700

    27

--- Assets/Script/TestScript.cs
+++ Assets/Script/TestScript.cs
@@ -15,6 +15,7 @@ public class TestScript : MonoBehaviour
         Debug.Log("Hi-6");
         Debug.Log("Hi-6");
         Debug.Log("Hi-6");
+        Debug.Log("Hi-6");
     }
 
     // Update is called once per frame
