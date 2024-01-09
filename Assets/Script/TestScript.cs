4223d72767934ee981f6bdd277476368784c8b56
nattapat.s <nattapat.s@varisoft.com>
2024-01-09 14:59:55 +0700

    init unity

---
+++ Assets/Script/TestScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi!");
+        Debug.Log("Hi-2");
+        Debug.Log("Hi-3");
+        Debug.Log("Hi-4");
+        Debug.Log("Hi-5");
+        Debug.Log("Hi-6");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
